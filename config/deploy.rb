1a3f65d36a8dafb38aaba2156bfbe2c9572685ca
Gustavo Cunha <gustavocunha@gmail.com>
2014-12-18 14:15:38 -0300

    Update deploy to ruby 2.1.5

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.3.5'
 set :application, 'admin.alagoasdevday.com.br'
 set :repo_url, 'git@github.com:alagoasdevday/admin.git'
 set :base_path, "/var/www/rails/admin.alagoasdevday.com.br"
-set :ruby_version, "2.1.4"
+set :ruby_version, "2.1.5"
 set :ruby_gemset, "admin.alagoasdevday.com.br"
 ask :user, "root"
 ask :server, "example.com"
